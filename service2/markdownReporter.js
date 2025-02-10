1606cfe45673029a9f74e3e1d008f6baac7056df
Andres Felipe Cardona Escobar <acardona@MacBook-Pro-de-Andres.local>
2025-02-10 12:39:13 -0500

    fix workflow

--- service2/markdownReporter.js
+++ service2/markdownReporter.js
@@ -13,16 +13,16 @@ class MarkdownReporter {
     }
 
     generateMarkdown(results) {
-        let markdown = "#Tests Results\n\n";
+        let markdown = "# Tests Results\n\n";
 
-        markdown += "##Summary\n";
+        markdown += "## Summary\n";
         markdown += `- **Total tests**: ${ results.numTotalTests }\n`;
         markdown += `- **Passed tests**: ${ results.numPassedTests }\n`;
         markdown += `- **Failed tests**: ${ results.numFailedTests }\n`;
         markdown += `- **Skipped tests**: ${ results.numPendingTests }\n`;
 
         results.testResults.forEach(suite => {
-            markdown += `${suite.testPathFile}\n`;
+            markdown += `# ${suite.testPathFile}\n`;
 
             suite.testResults.forEach(test => {
                 const icon = test.status === "passed" ? "✅": "❌";
